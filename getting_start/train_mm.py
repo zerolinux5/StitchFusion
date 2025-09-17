577e07748a428e42139536d3e7d4a1fb54633f1d
Jesus Magana <maganazero5@gmail.com>
2025-09-17 14:06:02 +0200

    linter

--- getting_start/train_mm.py
+++ getting_start/train_mm.py
@@ -1,5 +1,5 @@
 import os
-import torch 
+import torch
 import argparse
 import yaml
 import time
@@ -15,7 +15,7 @@ from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.utils.data import DistributedSampler, RandomSampler
 from torch import distributed as dist
 from semseg.models import *
-from semseg.datasets import * 
+from semseg.datasets import *
 from semseg.augmentations_mm import get_train_augmentation, get_val_augmentation
 from semseg.losses import get_loss
 from semseg.schedulers import get_scheduler
@@ -33,57 +33,85 @@ def main(cfg, gpu, save_dir):
     device = torch.device(cfg['DEVICE'])
     train_cfg, eval_cfg = cfg['TRAIN'], cfg['EVAL']
     dataset_cfg, model_cfg = cfg['DATASET'], cfg['MODEL']
-    loss_cfg, optim_cfg, sched_cfg = cfg['LOSS'], cfg['OPTIMIZER'], cfg['SCHEDULER']
+    loss_cfg, optim_cfg, sched_cfg = cfg['LOSS'], cfg['OPTIMIZER'], cfg[
+        'SCHEDULER']
     epochs, lr = train_cfg['EPOCHS'], optim_cfg['LR']
     resume_path = cfg['MODEL']['RESUME']
     gpus = int(os.environ['WORLD_SIZE'])
 
-    traintransform = get_train_augmentation(train_cfg['IMAGE_SIZE'], seg_fill=dataset_cfg['IGNORE_LABEL'])
+    traintransform = get_train_augmentation(
+        train_cfg['IMAGE_SIZE'], seg_fill=dataset_cfg['IGNORE_LABEL'])
     valtransform = get_val_augmentation(eval_cfg['IMAGE_SIZE'])
 
-    trainset = eval(dataset_cfg['NAME'])(dataset_cfg['ROOT'], 'train', traintransform, dataset_cfg['MODALS'])
-    valset = eval(dataset_cfg['NAME'])(dataset_cfg['ROOT'], 'val', valtransform, dataset_cfg['MODALS'])
+    trainset = eval(dataset_cfg['NAME'])(dataset_cfg['ROOT'], 'train',
+                                         traintransform, dataset_cfg['MODALS'])
+    valset = eval(dataset_cfg['NAME'])(dataset_cfg['ROOT'], 'val',
+                                       valtransform, dataset_cfg['MODALS'])
     class_names = trainset.CLASSES
 
-    model = eval(model_cfg['NAME'])(model_cfg['BACKBONE'], trainset.n_classes, dataset_cfg['MODALS'])
+    model = eval(model_cfg['NAME'])(model_cfg['BACKBONE'], trainset.n_classes,
+                                    dataset_cfg['MODALS'])
     resume_checkpoint = None
     if os.path.isfile(resume_path):
-        resume_checkpoint = torch.load(resume_path, map_location=torch.device('cpu'))
+        resume_checkpoint = torch.load(resume_path,
+                                       map_location=torch.device('cpu'))
         msg = model.load_state_dict(resume_checkpoint['model_state_dict'])
         # print(msg)
         logger.info(msg)
     else:
         model.init_pretrained(model_cfg['PRETRAINED'])
     model = model.to(device)
-    
+
     iters_per_epoch = len(trainset) // train_cfg['BATCH_SIZE'] // gpus
     loss_fn = get_loss(loss_cfg['NAME'], trainset.ignore_label, None)
     start_epoch = 0
-    optimizer = get_optimizer(model, optim_cfg['NAME'], lr, optim_cfg['WEIGHT_DECAY'])
-    scheduler = get_scheduler(sched_cfg['NAME'], optimizer, int((epochs+1)*iters_per_epoch), sched_cfg['POWER'], iters_per_epoch * sched_cfg['WARMUP'], sched_cfg['WARMUP_RATIO'])
+    optimizer = get_optimizer(model, optim_cfg['NAME'], lr,
+                              optim_cfg['WEIGHT_DECAY'])
+    scheduler = get_scheduler(sched_cfg['NAME'], optimizer,
+                              int((epochs + 1) * iters_per_epoch),
+                              sched_cfg['POWER'],
+                              iters_per_epoch * sched_cfg['WARMUP'],
+                              sched_cfg['WARMUP_RATIO'])
 
-    if train_cfg['DDP']: 
-        sampler = DistributedSampler(trainset, dist.get_world_size(), dist.get_rank(), shuffle=True)
+    if train_cfg['DDP']:
+        sampler = DistributedSampler(trainset,
+                                     dist.get_world_size(),
+                                     dist.get_rank(),
+                                     shuffle=True)
         sampler_val = None
-        model = DDP(model, device_ids=[gpu], output_device=0, find_unused_parameters=False)
+        model = DDP(model,
+                    device_ids=[gpu],
+                    output_device=0,
+                    find_unused_parameters=False)
     else:
         sampler = RandomSampler(trainset)
         sampler_val = None
-    
+
     if resume_checkpoint:
         start_epoch = resume_checkpoint['epoch'] - 1
         optimizer.load_state_dict(resume_checkpoint['optimizer_state_dict'])
         scheduler.load_state_dict(resume_checkpoint['scheduler_state_dict'])
-        loss = resume_checkpoint['loss']        
+        loss = resume_checkpoint['loss']
         best_mIoU = resume_checkpoint['best_miou']
-           
-    trainloader = DataLoader(trainset, batch_size=train_cfg['BATCH_SIZE'], num_workers=num_workers, drop_last=True, pin_memory=False, sampler=sampler)
-    valloader = DataLoader(valset, batch_size=eval_cfg['BATCH_SIZE'], num_workers=num_workers, pin_memory=False, sampler=sampler_val)
+
+    trainloader = DataLoader(trainset,
+                             batch_size=train_cfg['BATCH_SIZE'],
+                             num_workers=num_workers,
+                             drop_last=True,
+                             pin_memory=False,
+                             sampler=sampler)
+    valloader = DataLoader(valset,
+                           batch_size=eval_cfg['BATCH_SIZE'],
+                           num_workers=num_workers,
+                           pin_memory=False,
+                           sampler=sampler_val)
 
     scaler = GradScaler(enabled=train_cfg['AMP'])
-    if (train_cfg['DDP'] and torch.distributed.get_rank() == 0) or (not train_cfg['DDP']):
+    if (train_cfg['DDP']
+            and torch.distributed.get_rank() == 0) or (not train_cfg['DDP']):
         writer = SummaryWriter(str(save_dir))
-        logger.info('================== model complexity =====================')
+        logger.info(
+            '================== model complexity =====================')
         cal_flops(model, dataset_cfg['MODALS'], logger)
         logger.info('================== model structure =====================')
         logger.info(model)
@@ -94,17 +122,23 @@ def main(cfg, gpu, save_dir):
         model.train()
         if train_cfg['DDP']: sampler.set_epoch(epoch)
 
-        train_loss = 0.0        
+        train_loss = 0.0
         lr = scheduler.get_lr()
         lr = sum(lr) / len(lr)
-        pbar = tqdm(enumerate(trainloader), total=iters_per_epoch, desc=f"Epoch: [{epoch+1}/{epochs}] Iter: [{0}/{iters_per_epoch}] LR: {lr:.8f} Loss: {train_loss:.8f}")
-        metrics = Metrics(trainset.n_classes, trainloader.dataset.ignore_label, device)
+        pbar = tqdm(
+            enumerate(trainloader),
+            total=iters_per_epoch,
+            desc=
+            f"Epoch: [{epoch+1}/{epochs}] Iter: [{0}/{iters_per_epoch}] LR: {lr:.8f} Loss: {train_loss:.8f}"
+        )
+        metrics = Metrics(trainset.n_classes, trainloader.dataset.ignore_label,
+                          device)
 
         for iter, (sample, lbl) in pbar:
             optimizer.zero_grad(set_to_none=True)
             sample = [x.to(device) for x in sample]
             lbl = lbl.to(device)
-            
+
             with autocast(enabled=train_cfg['AMP']):
                 logits = model(sample)
                 loss = loss_fn(logits, lbl)
@@ -120,13 +154,16 @@ def main(cfg, gpu, save_dir):
             lr = scheduler.get_lr()
             lr = sum(lr) / len(lr)
             if lr <= 1e-8:
-                lr = 1e-8 # minimum of lr
+                lr = 1e-8  # minimum of lr
             train_loss += loss.item()
 
-            pbar.set_description(f"Epoch: [{epoch+1}/{epochs}] Iter: [{iter+1}/{iters_per_epoch}] LR: {lr:.8f} Loss: {train_loss / (iter+1):.8f}")
-        
-        train_loss /= iter+1
-        if (train_cfg['DDP'] and torch.distributed.get_rank() == 0) or (not train_cfg['DDP']):
+            pbar.set_description(
+                f"Epoch: [{epoch+1}/{epochs}] Iter: [{iter+1}/{iters_per_epoch}] LR: {lr:.8f} Loss: {train_loss / (iter+1):.8f}"
+            )
+
+        train_loss /= iter + 1
+        if (train_cfg['DDP'] and torch.distributed.get_rank()
+                == 0) or (not train_cfg['DDP']):
             writer.add_scalar('train/loss', train_loss, epoch)
         torch.cuda.empty_cache()
         _, miou = metrics.compute_iou()
@@ -139,9 +176,12 @@ def main(cfg, gpu, save_dir):
             "Train Pixel Acc": macc,
             "Train F1": mf1
         }
-        if ((epoch+1) % train_cfg['EVAL_INTERVAL'] == 0 and (epoch+1)>train_cfg['EVAL_START']) or (epoch+1) == epochs:
-            if (train_cfg['DDP'] and torch.distributed.get_rank() == 0) or (not train_cfg['DDP']):
-                acc, macc, _, mf1, ious, miou, val_loss = evaluate(model, valloader, device, loss_fn)                
+        if ((epoch + 1) % train_cfg['EVAL_INTERVAL'] == 0 and
+            (epoch + 1) > train_cfg['EVAL_START']) or (epoch + 1) == epochs:
+            if (train_cfg['DDP'] and torch.distributed.get_rank()
+                    == 0) or (not train_cfg['DDP']):
+                acc, macc, _, mf1, ious, miou, val_loss = evaluate(
+                    model, valloader, device, loss_fn)
                 writer.add_scalar('val/mIoU', miou, epoch)
                 writer.add_scalar('val/loss', val_loss, epoch)
                 log_data = {
@@ -158,36 +198,53 @@ def main(cfg, gpu, save_dir):
                     if os.path.isfile(prev_best): os.remove(prev_best)
                     if os.path.isfile(prev_best_ckp): os.remove(prev_best_ckp)
                     best_mIoU = miou
-                    best_epoch = epoch+1
+                    best_epoch = epoch + 1
                     cur_best_ckp = save_dir / f"{model_cfg['NAME']}_{model_cfg['BACKBONE']}_{dataset_cfg['NAME']}_epoch{best_epoch}_{best_mIoU}_checkpoint.pth"
                     cur_best = save_dir / f"{model_cfg['NAME']}_{model_cfg['BACKBONE']}_{dataset_cfg['NAME']}_epoch{best_epoch}_{best_mIoU}.pth"
-                    torch.save(model.module.state_dict() if train_cfg['DDP'] else model.state_dict(), cur_best)
-                    # --- 
-                    torch.save({'epoch': best_epoch,
-                                'model_state_dict': model.module.state_dict() if train_cfg['DDP'] else model.state_dict(),
-                                'optimizer_state_dict': optimizer.state_dict(),
-                                'loss': train_loss,
-                                'scheduler_state_dict': scheduler.state_dict(),
-                                'best_miou': best_mIoU,
-                                }, cur_best_ckp)
-                    logger.info(print_iou(epoch, ious, miou, acc, macc, class_names))
-                logger.info(f"Current epoch:{epoch} mIoU: {miou} Best mIoU: {best_mIoU}")
+                    torch.save(
+                        model.module.state_dict()
+                        if train_cfg['DDP'] else model.state_dict(), cur_best)
+                    # ---
+                    torch.save(
+                        {
+                            'epoch':
+                            best_epoch,
+                            'model_state_dict':
+                            model.module.state_dict()
+                            if train_cfg['DDP'] else model.state_dict(),
+                            'optimizer_state_dict':
+                            optimizer.state_dict(),
+                            'loss':
+                            train_loss,
+                            'scheduler_state_dict':
+                            scheduler.state_dict(),
+                            'best_miou':
+                            best_mIoU,
+                        }, cur_best_ckp)
+                    logger.info(
+                        print_iou(epoch, ious, miou, acc, macc, class_names))
+                logger.info(
+                    f"Current epoch:{epoch} mIoU: {miou} Best mIoU: {best_mIoU}"
+                )
 
-    if (train_cfg['DDP'] and torch.distributed.get_rank() == 0) or (not train_cfg['DDP']):
+    if (train_cfg['DDP']
+            and torch.distributed.get_rank() == 0) or (not train_cfg['DDP']):
         writer.close()
     pbar.close()
     end = time.gmtime(time.time() - start)
 
-    table = [
-        ['Best mIoU', f"{best_mIoU:.2f}"],
-        ['Total Training Time', time.strftime("%H:%M:%S", end)]
-    ]
+    table = [['Best mIoU', f"{best_mIoU:.2f}"],
+             ['Total Training Time',
+              time.strftime("%H:%M:%S", end)]]
     logger.info(tabulate(table, numalign='right'))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--cfg', type=str, default='configs/deliver_rgbdel.yaml', help='Configuration file to use')
+    parser.add_argument('--cfg',
+                        type=str,
+                        default='configs/deliver_rgbdel.yaml',
+                        help='Configuration file to use')
     args = parser.parse_args()
 
     with open(args.cfg) as f:
@@ -203,7 +260,7 @@ if __name__ == '__main__':
     exp_name = '_'.join([cfg['DATASET']['NAME'], model, modals])
     save_dir = Path(cfg['SAVE_DIR'], exp_name)
     if os.path.isfile(cfg['MODEL']['RESUME']):
-        save_dir =  Path(os.path.dirname(cfg['MODEL']['RESUME']))
+        save_dir = Path(os.path.dirname(cfg['MODEL']['RESUME']))
     os.makedirs(save_dir, exist_ok=True)
     logger = get_logger(save_dir / 'train.log')
     main(cfg, gpu, save_dir)
